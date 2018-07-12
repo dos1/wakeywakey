835a2bcbab8304e14ea4d26e9686e7943c6d065e
Sebastian Krzyszkowiak <dos@dosowisko.net>
2018-07-13 00:54:43 +0200

    basic movement and stuff

--- src/gamestates/board.c
+++ src/gamestates/board.c
@@ -21,26 +21,51 @@
 #include "../common.h"
 #include <libsuperderpy.h>
 
-static const float COLS = 6;
-static const float ROWS = 12;
+#define COLS 6.0
+#define ROWS 12.0
+
+struct Field {
+	bool bird;
+	bool cloud;
+};
+
+struct Player {
+	int position;
+	bool active;
+	bool visible;
+};
+
+struct Cloud {
+	int position;
+};
+
+struct Goose {
+	int position;
+};
 
 struct GamestateResources {
 	// This struct is for every resource allocated and used by your gamestate.
 	// It gets created on load and then gets passed around to all other function calls.
 
-	struct {
+	struct Layers {
 		ALLEGRO_BITMAP *bg, *fg, *water, *sky;
-	} bg;
+	} layers;
 
 	struct Tween camera;
 
-	bool cameraMove, showMenu;
+	bool cameraMove, showMenu, started;
 
 	ALLEGRO_BITMAP *logo, *menu;
 
-	struct {
+	struct Mouse {
 		float x, y;
 	} mouse;
+
+	struct Player players[4];
+
+	struct Player* currentPlayer;
+
+	struct Field board[(int)COLS * (int)ROWS];
 };
 
 int Gamestate_ProgressCount = 6; // number of loading steps as reported by Gamestate_Load; 0 when missing
@@ -48,6 +73,14 @@ int Gamestate_ProgressCount = 6; // number of loading steps as reported by Games
 static void HideMenu(struct Game* game, struct Tween* tween, void* d) {
 	struct GamestateResources* data = d;
 	data->showMenu = false;
+}
+
+static void ScrollCamera(struct Game* game, struct GamestateResources* data) {
+	data->cameraMove = true;
+	float cam = GetTweenValue(&data->camera);
+	float pos = data->currentPlayer->position;
+	pos /= COLS * ROWS;
+	data->camera = Tween(game, cam, 1.0 - pos, 2.5, TWEEN_STYLE_QUARTIC_IN_OUT);
 }
 
 void Gamestate_Logic(struct Game* game, struct GamestateResources* data, double delta) {
@@ -65,10 +98,10 @@ void Gamestate_Draw(struct Game* game, struct GamestateResources* data) {
 
 	float y = GetTweenValue(&data->camera);
 
-	al_draw_bitmap(data->bg.sky, 0, -1080 + 1080 * y * 0.1, 0);
-	al_draw_bitmap(data->bg.water, 1920 * Fract(al_get_time() / 6.0), 1439 + -1080 + 1080 * y * 1.2, 0);
-	al_draw_bitmap(data->bg.water, 1920 * Fract(al_get_time() / 6.0) - 1920, 1439 + -1080 + 1080 * y * 1.2, 0);
-	al_draw_bitmap(data->bg.bg, 0, 943 + -1080 + 1080 * y * 1.1, 0);
+	al_draw_bitmap(data->layers.sky, 0, -1080 + 1080 * y * 0.1, 0);
+	al_draw_bitmap(data->layers.water, 1920 * Fract(al_get_time() / 6.0), 1439 + -1080 + 1080 * y * 1.2, 0);
+	al_draw_bitmap(data->layers.water, 1920 * Fract(al_get_time() / 6.0) - 1920, 1439 + -1080 + 1080 * y * 1.2, 0);
+	al_draw_bitmap(data->layers.bg, 0, 943 + -1080 + 1080 * y * 1.1, 0);
 
 	// TODO: transforms are pretty cumbersome to use and restore; add some utils for them?
 
@@ -82,15 +115,19 @@ void Gamestate_Draw(struct Game* game, struct GamestateResources* data) {
 		al_draw_bitmap(data->logo, 0, 0, 0);
 		al_draw_bitmap(data->menu, 565, 574, 0);
 	} else {
-		for (int i = 0; i < COLS; i++) {
-			for (int j = 0; j < ROWS; j++) {
-				al_draw_filled_rounded_rectangle((i + 1) * 1920 / (COLS + 2) + 5, (j + 1) * 2160 / (ROWS + 2) + 3, (i + 2) * 1920 / (COLS + 2) - 5, (j + 2) * 2160 / (ROWS + 2) - 3, 20, 20, al_premul_rgba(0, 255, 255, 64));
+		for (int j = 0; j < ROWS; j++) {
+			for (int i = 0; i < COLS; i++) {
+				int num = j * (int)COLS + i;
+				if (j % 2) {
+					num = j * (int)COLS + ((int)COLS - i) - 1;
+				}
+				al_draw_filled_rounded_rectangle((i + 1) * 1920 / (COLS + 2) + 5, (j + 1) * 2160 / (ROWS + 2) + 3, (i + 2) * 1920 / (COLS + 2) - 5, (j + 2) * 2160 / (ROWS + 2) - 3, 20, 20, al_premul_rgba(0, 255, 255, data->board[num].bird ? 255 : 64));
 			}
 		}
 	}
 
 	al_use_transform(&orig);
-	al_draw_bitmap(data->bg.fg, 0, 423 + -1080 + 1080 * y * 1.5, 0);
+	al_draw_bitmap(data->layers.fg, 0, 423 + -1080 + 1080 * y * 1.5, 0);
 }
 
 void Gamestate_ProcessEvent(struct Game* game, struct GamestateResources* data, ALLEGRO_EVENT* ev) {
@@ -107,7 +144,7 @@ void Gamestate_ProcessEvent(struct Game* game, struct GamestateResources* data, 
 	}
 
 	if (ev->type == ALLEGRO_EVENT_KEY_DOWN) {
-		if (ev->keyboard.keycode == ALLEGRO_KEY_SPACE) {
+		if (ev->keyboard.keycode == ALLEGRO_KEY_SPACE && !data->started) {
 			data->cameraMove = true;
 			data->camera.callback = HideMenu;
 		}
@@ -115,9 +152,21 @@ void Gamestate_ProcessEvent(struct Game* game, struct GamestateResources* data, 
 			StopCurrentGamestate(game);
 			StartGamestate(game, "board");
 		}
-		if (ev->keyboard.keycode == ALLEGRO_KEY_BACKSPACE) {
+		if (ev->keyboard.keycode == ALLEGRO_KEY_BACKSPACE && game->config.debug) {
 			data->cameraMove = true;
-			data->camera = Tween(game, 0.0, 0.5, 3.0, TWEEN_STYLE_QUARTIC_IN_OUT);
+			data->camera = Tween(game, 0.0, 1.0, 3.0, TWEEN_STYLE_QUARTIC_IN_OUT);
+		}
+		if (ev->keyboard.keycode == ALLEGRO_KEY_LEFT && game->config.debug) {
+			data->board[data->players[0].position].bird = false;
+			data->players[0].position--;
+			data->board[data->players[0].position].bird = true;
+			ScrollCamera(game, data);
+		}
+		if (ev->keyboard.keycode == ALLEGRO_KEY_RIGHT && game->config.debug) {
+			data->board[data->players[0].position].bird = false;
+			data->players[0].position++;
+			data->board[data->players[0].position].bird = true;
+			ScrollCamera(game, data);
 		}
 	}
 }
@@ -132,13 +181,13 @@ void* Gamestate_Load(struct Game* game, void (*progress)(struct Game*)) {
 	struct GamestateResources* data = calloc(1, sizeof(struct GamestateResources));
 	progress(game); // report that we progressed with the loading, so the engine can move a progress bar
 
-	data->bg.bg = al_load_bitmap(GetDataFilePath(game, "bg/bg.png"));
+	data->layers.bg = al_load_bitmap(GetDataFilePath(game, "bg.png"));
 	progress(game);
-	data->bg.fg = al_load_bitmap(GetDataFilePath(game, "bg/fg.png"));
+	data->layers.fg = al_load_bitmap(GetDataFilePath(game, "fg.png"));
 	progress(game);
-	data->bg.sky = al_load_bitmap(GetDataFilePath(game, "bg/sky.png"));
+	data->layers.sky = al_load_bitmap(GetDataFilePath(game, "sky.png"));
 	progress(game);
-	data->bg.water = al_load_bitmap(GetDataFilePath(game, "bg/water.png"));
+	data->layers.water = al_load_bitmap(GetDataFilePath(game, "water.png"));
 	progress(game);
 	data->logo = al_load_bitmap(GetDataFilePath(game, "logo.png"));
 	progress(game);
@@ -160,6 +209,12 @@ void Gamestate_Start(struct Game* game, struct GamestateResources* data) {
 	data->camera.data = data;
 	data->cameraMove = false;
 	data->showMenu = true;
+	data->started = false;
+
+	data->board[0].bird = true;
+	data->players[0].position = 0;
+
+	data->currentPlayer = &data->players[0];
 }
 
 void Gamestate_Stop(struct Game* game, struct GamestateResources* data) {
